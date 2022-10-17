d7bb247c4de9b8045c63927da14ff4375f1acb32
Otabek Mavlonov <mavlonovo@lenovolaptop.localdomain>
2022-10-17 13:03:00 -0500

    first_commit

---
+++ fn.h
@@ -0,0 +1,12 @@
+#ifndef PASSING_BY_REFERENCE
+#define PASSING_BY_REFERENCE
+struct example
+{
+	int value =2;
+};
+
+void function_one(/*Fill with the appropriate arguments */);
+void function_two(/*Fill with the appropriate arguments */);
+void function_three(int *k, int &l);
+void function_four(/*Fill with the appropriate arguments */);
+#endif
